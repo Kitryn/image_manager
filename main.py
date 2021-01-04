81efda7781b4270b0996fcc42410e8fa4e5a73b5
Kitryn <snowfennard@gmail.com>
2021-01-04 19:09:51 +0800

    Added function to look up prefs in db

--- main.py
+++ main.py
@@ -28,10 +28,17 @@ def file_checksum(file):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+def get_preference(pref):
+    # Looks up the db and returns a value for the selected pref
+    with conn:
+        cur = conn.execute("SELECT pref, val FROM prefs WHERE(pref=?)",(pref,))
+        print(cur.fetchone())
+
 def add_metadata_to_db(file_list):
     # Takes a iterable of Path objects of files to add
     # TODO: try/error bc it likely will shout at you if you insert duplicates
     # Prepare iterable: fname, fpath, fhash
+    # TODO: probably need to change table layout to deal with dupe checksums
     metadata = [(file.name, str(file.parent), file_checksum(file)) for file in file_list]
     
     with conn:
@@ -40,5 +47,6 @@ def add_metadata_to_db(file_list):
 
 if __name__ == "__main__":
     init_db()
-    add_metadata_to_db(walk_directory(Path(".")))
+    # add_metadata_to_db(walk_directory(Path(".")))
+    get_preference("gallery_root")
     
