7b96ca147a38aaf8ea39c39840f30acac4658f65
Kitryn <snowfennard@gmail.com>
2021-01-04 19:34:49 +0800

    Refactored schema to be just path instead of path+name, and be unique on this value instead of hash

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import hashlib
 conn = sqlite3.connect("example.db")
 
 def walk_directory(root_directory):
-    # Returns a tuple? of all files and their absolute paths, excludes dotfiles and dirs starting with dot
-    # This has to use relative directories! can maybe expand to absolutes elsewhere if required
+    # Returns a tuple? of all files and their paths, excludes dotfiles and dirs starting with dot
+    # This uses relative directories, can maybe expand to absolutes elsewhere if required
     def file_filter(path_obj):
+        # Filter Paths we don't want, like directories or dotfiles
         if not path_obj.is_file(): return False
         for parent_dir in path_obj.parents:
             if str(parent_dir.name).startswith("."): return False
         return True
+
     return (file for file in Path(root_directory).glob("**/[!.]*") if file_filter(file))  # this can be a generator
 
 def init_db():
@@ -37,16 +39,15 @@ def get_preference(pref):
 def add_metadata_to_db(file_list):
     # Takes a iterable of Path objects of files to add
     # TODO: try/error bc it likely will shout at you if you insert duplicates
-    # Prepare iterable: fname, fpath, fhash
-    # TODO: probably need to change table layout to deal with dupe checksums
-    metadata = [(file.name, str(file.parent), file_checksum(file)) for file in file_list]
+    # Prepare iterable: fpath, fhash
+    metadata = [(str(file), file_checksum(file)) for file in file_list]
     
     with conn:
-        conn.executemany("INSERT INTO metadata(fname, fpath, fhash) VALUES(?, ?, ?)", metadata)
+        conn.executemany("INSERT INTO metadata(fpath, fhash) VALUES(?, ?)", metadata)
         
 
 if __name__ == "__main__":
     init_db()
-    # add_metadata_to_db(walk_directory(Path(".")))
+    add_metadata_to_db(walk_directory(Path(".")))
     get_preference("gallery_root")
     
